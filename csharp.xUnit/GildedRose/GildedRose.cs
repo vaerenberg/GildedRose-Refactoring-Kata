249aa712a8ef34a5bcaa2423e97a32c0168ee86f
Bart Vaerenberg <vaerenberg@gmail.com>
2024-11-27 09:48:40 +0100

    simplify

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -15,15 +15,13 @@ public class GildedRose
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            var daylyDegradation = Items[i].Name.StartsWith("Conjured") ? 2 : 1;
-
             if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
             {
                 if (Items[i].Quality > 0)
                 {
                     if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
                     {
-                        Items[i].Quality = Items[i].Quality - daylyDegradation;
+                        Items[i].Quality -= Items[i].Name.StartsWith("Conjured") ? 2 : 1;
                     }
                 }
             }
